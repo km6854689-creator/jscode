b959b14b9fb4f250bff201b0791cd3d5d354154a
km6854689-creator <km6854689@gmail.com>
2026-08-12 14:34:52 +0000

    container config done

---
+++ .devcontainer/01/variable.js
@@ -0,0 +1,14 @@
+const accountId = 7107
+let accountEmail ="madhu@gmail.com"
+var accountPassword ="12345"
+accountCity = "banglore"
+let accountState;
+
+// accountId=2
+
+accountEmail ="he@ha.com"
+accountPassword ="13335"
+accountCity ="delhi"
+
+
+console.table([accountEmail,accountId , accountPassword , accountCity, accountState])
